9f9ea264d9c553cf9adfe0c940d71c2f5889085c
eOnofri04 <elia.onofri@mensa.it>
2019-04-18 15:19:04 +0200

    Continue Example 5

--- src/linear_cost.m
+++ src/linear_cost.m
@@ -1,11 +1,13 @@
 %linear_cost Compute the mean square error of the hypotesis function
 %   Evaluate the mean square error of the hypothesis function `h`:
 %   ```math
-%       h_\theta(x) = \theta_0 + x_1\theta_1 + \dots + x_n\theta_n = X\theta
+%       h_\theta(x) =
+%       \theta_0 + x_1\theta_1 + \dots + x_n\theta_n =
+%       X\theta 
 %   ```
 %   from the `m` effective value `Y` of the training set.
 %
-%   @param Xo     : `m` by `n+1` matrix of the Training Set Features.
+%   @param Xo     : `m` by `n+1` matrix of the Training Set Features;
 %                   each row represent a sample with `n` features.
 %   @param Y      : `m` column vector of the Training Set Samples.
 %   @param theta  : `n+1` column vector of the hypotesis coefficient
--- src/neural_cost.m
+++ src/neural_cost.m
@@ -1,19 +1,56 @@
-function [ J, J_grad ] = neural_cost( Theta, s, X, Y, lambda)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%neural_cost Compute neural network cost function and associated gradient.
+%   Evaluate the cost function by applying:
+%    - forward propagation of the training set for each training sample
+%       ```math
+%           a^{(l+1)} = g(\Theta^{(l)} * a^{(l)}),
+%           \qquad \forall l \in[1,L]
+%       ```
+%    - error evaluation of the last layer
+%    - error backward propagation of the error
+%    - evaluation of the gradient
+%
+%   @param Theta  : `L-1` Cell-row-vector of the weight matrices between
+%                   eachcouple of layers. `Theta{l}(i j)` is the weight
+%                   from `i`-th unit in the `l` layer to the `j`-th unit
+%                   in the`l+1` layer.
+%   @param s      : `L` row-vector of the number of units in each layer.
+%   @param X      : `m` by `n` matrix of the Training Set Features;
+%                   each row represents a sample with `n` features.
+%   @param Y      : `m` column vector of the Training Set Samples.
+%   @param lambda : 
+%
+%   @return J     : Cost function for the choosen `theta`
+%   @return J_grad: `n+1` column vector of the gradient value of `J`
 
-[t1, t2, L] = size(Theta);
-[m, n] = size(X);
+function [ J, J_grad ] = neural_cost( Theta, s, X, Y, lambda)
+%% Parameters Initialization
+[~, L] = size(s);
+[m, ~] = size(X);
+a = cell(L, 1);
+
+%% Input criteria check
 assert(size(s, 1) == 1);
-assert(max(s(1:length(s)-1)) == t1);
-assert(max(s(2:length(s))) == t2);
+assert(length(Theta) == L-1);
+for i = 1 : L-1
+    assert(isequal(size(Theta{i}), [s(i)+1 s(i+1)]))
+end
+assert(size(X, 2) == s(1));
 assert(isequal(size(Y), [m, 1]));
 
-for t = 1 : m
-    a(:,1) = X(t, :);
-    a(:,l+1) = sigmf(Theta(:,:,l) * a(:, l), [1 0]);
-
-J = 0;
-J_grad = 0;
+%% Forward Propagation
+a{1} = [ones(m, 1) X];  % bias juxtaposing
+for l = 1 : L-1
+    a{l+1} = sigmf(Theta{l} * a{l}, [1 0]);
 end
 
+%% Error Evaluation on L
+
+%% Error Backward Propagation
+
+%% Gradient Evaluation
+
+%% Output
+J = 0;
+J_grad = 0;
+
+end
