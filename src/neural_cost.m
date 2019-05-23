7a0b53d19619c9c5f92d65f7277fd4bab67ea71d
eOnofri04 <elia.onofri@mensa.it>
2019-05-23 11:34:45 +0200

    Removing Duplicates

--- src/neural_cost.m
+++ src/neural_cost.m
@@ -1,4 +1,4 @@
-%neural_cost Compute neural network cost function and associated gradient.
+%NEURAL_COST Compute neural network cost function and associated gradient.
 %   Evaluate the cost function by applying:
 %    - forward propagation of the training set for each training sample
 %       ```math
@@ -10,7 +10,7 @@
 %    - evaluation of the gradient
 %
 %   @param Theta  : `L-1` Cell-row-vector of the weight matrices between
-%                   eachcouple of layers. `Theta{l}(i j)` is the weight
+%                   each couple of layers. `Theta{l}(i j)` is the weight
 %                   from `i`-th unit in the `l` layer to the `j`-th unit
 %                   in the`l+1` layer.
 %   @param s      : `L` row-vector of the number of units in each layer.
@@ -23,6 +23,7 @@
 
 function [ J, grad ] = neural_cost( Theta, s, X, y, lambda )
 %% Parameters Initialization
+    changing_bias = 0;
     [~, L] = size(s);
     [m, ~] = size(X);
     
@@ -30,8 +31,8 @@ function [ J, grad ] = neural_cost( Theta, s, X, y, lambda )
     delta = cell(L, 1);
     grad = cell(L, 1);
     
-    Y = zeros(m, size(Theta{L-1}, 2));
-    Y(sub2ind(size(Y), 1:5000,y')) = 1;
+    Y = zeros(m, s(L));
+    Y(sub2ind([m s(L)], 1:m,y')) = 1;
     
     Theta_biasless = cell(L-1, 1);
     for l = 1 : L-1
@@ -63,7 +64,7 @@ function [ J, grad ] = neural_cost( Theta, s, X, y, lambda )
     reg = 0;
     if lambda ~= 0
         for l = 1 : L-1
-            reg = reg + sum(sum(Theta_biasless{l}.^2));
+            reg = reg + sum(sum(Theta{l}.^2));
         end
     end
     J = (lambda/2 * reg - J) / m;
@@ -71,15 +72,31 @@ function [ J, grad ] = neural_cost( Theta, s, X, y, lambda )
     delta{L} = a{L} - Y;
     
 %% Error Backward Propagation
-    for l = L-1 : -1 : 2
-        delta{l} = a{l}(:, 2:end) .* (1 - a{l}(:, 2:end)) .* (delta{l+1} * Theta_biasless{l}');
+    if changing_bias == 1
+        delta{L-1} = a{L-1} .* (1 - a{L-1}) .* (delta{L} * Theta{L-1}');
+        for l = L-2 : -1 : 2
+            delta{l} = a{l} .* (1 - a{l}) .* (delta{l+1}(:, 2:end) * Theta{l}');
+        end
+    else
+        for l = L-1 : -1 : 2
+            delta{l} = a{l}(:, 2:end) .* (1 - a{l}(:, 2:end)) .* (delta{l+1} * Theta_biasless{l}');
+        end
     end
     
 %% Gradient Evaluation
-    for l = 2 : L
-        grad{l-1} = a{l-1}(:, 2:end)' * delta{l};
+%     for l = 2 : L
+%         %grad{l-1} = a{l-1}' * delta{l};
+%         grad{l-1} = a{l-1}(:, 2:end)' * delta{l};
+%     end
+    Theta1_grad = zeros(size(Theta_biasless{1}));
+    Theta2_grad = zeros(size(Theta_biasless{2}));
+    for i = 1 : m
+        Theta1_grad = Theta1_grad + (delta{2}(i, :)' * a{1}(i, 2:end))';
+        Theta2_grad = Theta2_grad + (delta{3}(i, :)' * a{2}(i, 2:end))';
     end
-    
+    T1 = Theta1_grad'/m;
+    T2 = Theta1_grad'/m;
+    grad = [T1(:); T2(:)];
 %% Output
 
 end
